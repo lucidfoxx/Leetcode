7dae4eea6a8ea4a06ab5c009cdbb095a4717aeaf
Noor <sahinoor.afiya@gmail.com>
2026-08-08 00:07:30 +0530

    Time: 0 ms (100%), Space: 19.9 MB (81.57%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -7,7 +7,7 @@ class Solution:
                 if target == nums[i]:
                     return i
         for i in range(len(nums) - 1):
-            if(i==0 and target <= nums[i]):
+            if(i==0 and target < nums[i]):
                 return 0
             if nums[i] < target and target < nums[i + 1]:
                 return i + 1

14995771be71e71ce404d614e7541d9abe513e74
Noor <sahinoor.afiya@gmail.com>
2026-08-08 10:13:53 +0530

    Time: 1159 ms (15.58%), Space: 19.3 MB (57.41%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x ==1 :
+            return x
+        i = 2
+        while(i*i <= x):
+            i+=1
+        return (i-1)
+        
